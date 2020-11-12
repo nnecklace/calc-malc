b6e85405e4a0b0fcbecda7c820ee5fd16e548525
Niclas Forsman <niclas.forsman@futurice.com>
2020-11-12 19:04:33 +0200

    testing out what the tree structure would look like

--- app/src/main/java/calcmalc/App.java
+++ app/src/main/java/calcmalc/App.java
@@ -3,9 +3,11 @@
  */
 package calcmalc;
 
-import calcmalc.structures.List;
+import calcmalc.structures.ASTNode;
+import calcmalc.structures.Listable;
 import calcmalc.structures.Stack;
-import calcmalc.structures.Queue;
+import calcmalc.logic.types.Token;
+import calcmalc.logic.Lexer;
 import calcmalc.logic.Parser;
 import java.text.ParseException;
 import java.util.Scanner;
@@ -17,21 +19,45 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        Stack<String> stack = new Stack<>(new List<String>());
-        Queue<String> queue = new Queue<>(new List<String>());
-        Parser parser = new Parser(stack, queue);
+        Parser parser = new Parser();
         Scanner input = new Scanner(System.in);
+        Lexer lexer = new Lexer();
 
         while (input.hasNextLine()) {
-            String line = input.nextLine();
+            String line = input.nextLine().replaceAll("\\s", "");
 
             try {
-                parser.parse(line);
+                Listable<Token> tokens = lexer.lex(line);
+                Stack<ASTNode> nodes = parser.parse(tokens);
+                System.out.println(parser.printTree());
             } catch (ParseException e) {
                 System.out.println(e.getMessage());
             }
-
-            System.out.println(parser.show());
+            System.out.println();
         }
+    }
+
+    public static int dfs(ASTNode node) {
+        if (node.token().isNumber()) {
+            return Integer.parseInt(node.token().getKey());
+        }
+        int child = 0;
+        int value = 0;
+        int prod = 1;
+        while (node.getChildren().get(child) != null) {
+            if (node.token().getKey().equals("+")) {
+                value += dfs(node.getChildren().get(child));
+            }
+            else if (node.token().getKey().equals("*")) {
+                prod *= dfs(node.getChildren().get(child));
+            }
+            child++;
+        }
+
+        if (node.token().getKey().equals("+")) {
+            return value;
+        }
+
+        return prod;
     }
 }
