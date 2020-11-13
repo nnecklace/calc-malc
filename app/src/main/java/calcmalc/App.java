e58f91ea7937dc30a884598696e487784a083b99
Niclas Forsman <niclas.forsman@futurice.com>
2020-11-13 10:13:49 +0200

    refactor app to pass queue to parser

--- app/src/main/java/calcmalc/App.java
+++ app/src/main/java/calcmalc/App.java
@@ -6,6 +6,7 @@ package calcmalc;
 import calcmalc.structures.ASTNode;
 import calcmalc.structures.Listable;
 import calcmalc.structures.Stack;
+import calcmalc.structures.Queue;
 import calcmalc.logic.types.Token;
 import calcmalc.logic.Lexer;
 import calcmalc.logic.Parser;
@@ -19,7 +20,6 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
-        Parser parser = new Parser();
         Scanner input = new Scanner(System.in);
         Lexer lexer = new Lexer();
 
@@ -28,7 +28,8 @@ public class App {
 
             try {
                 Listable<Token> tokens = lexer.lex(line);
-                Stack<ASTNode> nodes = parser.parse(tokens);
+                Parser parser = new Parser(new Queue<Token>(tokens));
+                Stack<ASTNode> nodes = parser.parse();
                 System.out.println(parser.printTree());
             } catch (ParseException e) {
                 System.out.println(e.getMessage());
