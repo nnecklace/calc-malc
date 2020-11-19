cb0372f0d880c94636a57355adcf0e7eedefce78
Niclas Forsman <niclas.forsman@futurice.com>
2020-11-19 11:35:03 +0200

    create custom lexer exception

---
+++ app/src/main/java/calcmalc/exceptions/LexerException.java
@@ -0,0 +1,12 @@
+package calcmalc.exceptions;
+
+public class LexerException extends Exception {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public LexerException(String message) {
+        super(message);
+    }
+}
