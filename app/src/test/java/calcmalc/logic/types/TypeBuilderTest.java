1b246db3408f2f93fb385a9c09fa6c51ff14f025
Niclas Forsman <niclas.forsman@futurice.com>
2020-12-14 02:01:32 +0200

    add typebuilder tests

--- app/src/test/java/calcmalc/logic/types/TypeBuilderTest.java
+++ app/src/test/java/calcmalc/logic/types/TypeBuilderTest.java
@@ -1,6 +1,7 @@
 package calcmalc.logic.types;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -15,5 +16,17 @@ public class TypeBuilderTest {
         assertEquals(1, actual2.getPrecedence());
         assertEquals(3, actual3.getPrecedence());
         assertEquals(4, actual4.getPrecedence());
+    }
+
+    @Test
+    public void testTypeBuilderReturnsCorrectTokenType() {
+        Token actual1 = TypeBuilder.buildToken(Types.OPERATOR, "*");
+        Token actual2 = TypeBuilder.buildToken(Types.ASSIGNMENT, "=");
+        Token actual3 = TypeBuilder.buildToken(Types.VARIABLE_DELIMITER, ":");
+        Token actual4 = TypeBuilder.buildToken(Types.CLOSING_PARENTHESIS, "(");
+        assertTrue(actual1.isOperator() && !actual1.isVariableDelimiter()); // second part is only for a jacoco bug
+        assertTrue(actual2.isAssignment() && !actual2.isClosingParenthesis());
+        assertTrue(actual3.isVariableDelimiter());
+        assertTrue(actual4.isClosingParenthesis());
     }
 }
